fc71a82e26ae7ca708b8c894442dc8a2aed4a63f
pete <ptrmutie@gmail.com>
2024-03-15 01:54:09 -0700

    d

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,30 +1,14 @@
-#!/usr/bin/env python3
-
-import sys
+#!/usr/bin/python3
 import requests
 
+
 def number_of_subscribers(subreddit):
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'MyBot/0.0.1'}  # Set a custom User-Agent
-    
-    try:
-        response = requests.get(url, headers=headers)
-        if response.status_code == 200:
-            data = response.json()
-            subscribers = data['data']['subscribers']
-            return subscribers
-        else:
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0;\
+                         rv:68.0) Gecko/20100101 FirefoxFirefox/68.0'}
+    with requests.session() as client:
+        info = client.get(url, headers=headers, allow_redirects=False).json()
+        try:
+            return info.get('data', {}).get('subscribers', 0)
+        except Exception:
             return 0
-    except Exception as e:
-        print("Error:", e)
-        return 0
-
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: ./reddit_subscribers.py <subreddit>")
-        sys.exit(1)
-
-    subreddit_name = sys.argv[1]
-    subscribers_count = number_of_subscribers(subreddit_name)
-    print(f"The subreddit '{subreddit_name}' has {subscribers_count} subscribers.")
-
